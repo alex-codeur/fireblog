e4683d065e5436731560dc44babe003f2f79e22d
alex <apachecordovax@gmail.com>
2021-07-31 14:21:54 +0200

    mise en place de page d'edition d'un profile

--- src/store/index.js
+++ src/store/index.js
@@ -58,6 +58,15 @@ export default new Vuex.Store({
         state.profileFirstName.match(/(\b\S)?/g).join("") +
         state.profileLastName.match(/(\b\S)?/g).join("");
     },
+    changeFirstName(state, payload) {
+      state.profileFirstName = payload;
+    },
+    changeLastName(state, payload) {
+      state.profileLastName = payload;
+    },
+    changeUserName(state, payload) {
+      state.profileUserName = payload;
+    },
   },
   actions: {
     async getCurrentUser({ commit }) {
