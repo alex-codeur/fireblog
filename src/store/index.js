9ede292b848ac250a764fd506f0833ef00a57a4d
alex <apachecordovax@gmail.com>
2021-08-01 18:58:56 +0200

    firebase: suppression d'un post

--- src/store/index.js
+++ src/store/index.js
@@ -51,6 +51,9 @@ export default new Vuex.Store({
     toggleEditPost(state, payload) {
       state.editPost = payload;
     },
+    filterBlogPost(state, payload) {
+      state.blogPosts = state.blogPosts.filter((post) => post.blogID !== payload);
+    },
     updateUser(state, payload) {
       state.user = payload;
     },
@@ -100,6 +103,11 @@ export default new Vuex.Store({
         }
       });
       state.postLoaded = true;
+    },
+    async deletePost({ commit }, payload) {
+      const getPost = await db.collection("blogPosts").doc(payload);
+      await getPost.delete();
+      commit("filterBlogPost", payload);
     },
     async updateUserSettings({ commit, state }) {
       const dataBase = await db.collection("users").doc(state.profileId);
